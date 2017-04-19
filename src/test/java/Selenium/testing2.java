93997da4596369ad2bbd1f7148ee8d69e297e87d
Revanth <revanthkng@gmail.com>
2017-04-19 18:53:11 -0400

    mavenTest entire code

--- src/test/java/Selenium/testing2.java
+++ src/test/java/Selenium/testing2.java
@@ -17,7 +17,7 @@ public class testing2
 	public void testig()
 	{
 		System.out.println("test Page 3 ");
-		System.setProperty("webdriver.gecko.driver", "D:\\Driver Browsers\\geckodriver.exe");
+		System.setProperty("webdriver.gecko.driver", "D:\\Driver Browsers\\geckodriver0.14.exe");
 		WebDriver driver = new FirefoxDriver();
 		driver.get("http://www.ucmo.edu");
 		System.out.println(driver.getCurrentUrl());
