fc65a68c412d9a80eddbf9df0e543a928c5bb24b
Revanth <revanthkng@gmail.com>
2017-04-19 19:05:00 -0400

    Add log4j for maven test

--- src/test/java/Selenium/MavenTest.java
+++ src/test/java/Selenium/MavenTest.java
@@ -3,6 +3,8 @@
  */
 package Selenium;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
@@ -13,16 +15,22 @@ import org.testng.annotations.Test;
  */
 public class MavenTest 
 {
+	Logger logger;
 	@Test
 	public void maven()
 	{
-		System.setProperty("webdriver.gecko.driver", "D:\\Driver Browsers\\geckodriver.exe");
+		PropertyConfigurator.configure("D:\\maven\\MavenProject\\log4j.properties");
+		logger = Logger.getLogger("MevenTest with Log4j");
+		System.setProperty("webdriver.gecko.driver", "D:\\Driver Browsers\\geckodriver0.14.exe");
 		System.out.println("Maven test getting google");
 		WebDriver driver = new FirefoxDriver();
+		logger.info("Browser Intialized");
 		driver.get("http://www.google.com");
+		logger.info("Google home page Opened");
 		System.out.println("Hello it is maven Sample test");
+		logger.info("Browser about to close");
 		driver.close();
-		
+		logger.info("Browser closed");
 		
 		
 	}
