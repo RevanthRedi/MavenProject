edc80a5e1dae9a43d5c821f7360f1abfe1f57536
RevanthRedi <revanthkng@gmail.com>
2017-05-24 23:35:58 -0400

    updated imp changes

--- src/test/java/Selenium/gmail_Automation.java
+++ src/test/java/Selenium/gmail_Automation.java
@@ -55,7 +55,7 @@ public class gmail_Automation
 		driver.findElement(By.xpath(".//*[@id='identifierNext']/div[2]")).click();
 		Thread.sleep(5000);
 		logger.info("Password Page");
-		driver.findElement(By.xpath(".//*[@id='password']/div[1]/div/div[1]/input")).sendKeys("9160501501");
+		driver.findElement(By.xpath(".//*[@id='password']/div[1]/div/div[1]/input")).sendKeys("**********");
 //		
 		driver.findElement(By.xpath(".//*[@id='passwordNext']/content/span")).click();
 		logger.info("Signed in Successfully");
